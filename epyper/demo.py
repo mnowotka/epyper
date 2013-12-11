65f196679249eec69d6d81a73e323c02be108245
mnowotka <mnowotka@candela.(none)>
2013-12-11 22:07:56 +0000

    bugfix

--- epyper/demo.py
+++ epyper/demo.py
@@ -14,7 +14,7 @@ def demo():
     while True:
 
         print "EA Image"
-        eaLogo = Image.open(os.path.join(os.path.dirname(os.path.abspath(epyper.__file__))), "eaLogo.png"))
+        eaLogo = Image.open(os.path.join(os.path.dirname(os.path.abspath(epyper.__file__)), "eaLogo.png"))
         eaLogo = eaLogo.transpose(Image.FLIP_LEFT_RIGHT)
         eaData = list(eaLogo.rotate(180).getdata())
         displayImg(Display.EPD_TYPE_270, eaData, oldImg)
@@ -23,7 +23,7 @@ def demo():
         bsp.delayMs(10000)
 
         print "PD Image"
-        eaLogo = Image.open(os.path.join(os.path.dirname(os.path.abspath(epyper.__file__))), "pdLogo.png"))
+        eaLogo = Image.open(os.path.join(os.path.dirname(os.path.abspath(epyper.__file__)), "pdLogo.png"))
         pdLogo = pdLogo.transpose(Image.FLIP_LEFT_RIGHT)
         pdData = list(pdLogo.rotate(180).getdata())
         displayImg(Display.EPD_TYPE_270, pdData, oldImg)
