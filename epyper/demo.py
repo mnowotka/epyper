c9afd95f48390d2776df9d5db7f0106a5a10f9e2
mnowotka <mnowotka@candela.(none)>
2013-12-12 18:56:28 +0000

    making this bit more high-level

--- epyper/demo.py
+++ epyper/demo.py
@@ -1,43 +1,31 @@
-from epyper.displayController import displayImg
+from epyper.displayController import DisplayController
 from epyper.displayCOGProcess import Display
 from epyper import bsp
 from PIL import Image
 import epyper
 import os
-import itertools
-
-#-------------------------------------------------------------------------------
-
-def toBytes(bits):
-    return [sum([[0,1][byte[b]>0] << (7-b) for b in range(0,8)])
-               for byte in zip(*(iter(bits),) * 8)
-        ]
 
 #-------------------------------------------------------------------------------
 
 def demo():
     print "Starting E-paper demo"
-
-    bsp.init()
-    oldImg = bytearray(176*33)
+    display = DisplayController(Display.EPD_TYPE_270)
 
     while True:
 
         print "EA Image"
         eaLogo = Image.open(os.path.join(os.path.dirname(
                             os.path.abspath(epyper.__file__)), "eaLogo.png"))
-        eaData = toBytes(bytearray(eaLogo.convert("1").getdata()))
-        displayImg(Display.EPD_TYPE_270, eaData, oldImg)
-        oldImg = eaData
+                            
+        display.displayImg(eaLogo)
 
         bsp.delayMs(10000)
 
         print "PD Image"
         pdLogo = Image.open(os.path.join(os.path.dirname(
                             os.path.abspath(epyper.__file__)), "pdLogo.png"))
-        pdData = toBytes(bytearray(pdLogo.convert("1").getdata()))
-        displayImg(Display.EPD_TYPE_270, pdData, oldImg)
-        oldImg = pdData
+                            
+        display.displayImg(pdLogo)
 
         bsp.delayMs(10000)
         
--- epyper/displayController.py
+++ epyper/displayController.py
@@ -1,18 +1,50 @@
 from epyper.displayCOGProcess import Display
+from epyper import bsp
 
-def displayImg(EPDType, newImg, prevImg):
-    display = Display(EPDType)
-    
-    try:
-        # always initialize display (has been powered off before)
-        display.hwInit()
-        display.powerOn()
-        display.initializeDriver()
+#-------------------------------------------------------------------------------
 
-        # display new picture, but first remove old one
-        display.display(newImg, prevImg)
+class DisplayController():
+
+#-------------------------------------------------------------------------------
+
+    def __init__(EPDType)
+        bsp.init()
+        self.display = Display(EPDType)
+        params = self.display.COG_Params
+        size = params.horizontal * params.vertical
+        self.prevImg = bytearray(size)
+        self.newImg = bytearray(size)
+        self.size = (params.horizontal, params.vertical)
+
+#-------------------------------------------------------------------------------
         
-    finally:    
-        # power down display - picture still displayed
-        display.powerOff()
+    def toBytes(self, bits):
+        return [sum([[0,1][byte[b]>0] << (7-b) for b in range(0,8)])
+                   for byte in zip(*(iter(bits),) * 8)
+            ]    
+
+#-------------------------------------------------------------------------------
+
+    def displayImg(img):
+        if img.size != self.size:
+            print "image has a wrong size, should be %s" % str(self.size)
+            return
+            
+        self.newImg = self.toBytes(bytearray(img.convert("1").getdata()))
+        
+        try:
+            # always initialize display (has been powered off before)
+            self.display.hwInit()
+            self.display.powerOn()
+            self.display.initializeDriver()
+
+            # display new picture, but first remove old one
+            self.display.display(self.newImg, self.prevImg)
+            self.prevImg = self.newImg
+            
+        finally:    
+            # power down display - picture still displayed
+            self.display.powerOff()
+
+#-------------------------------------------------------------------------------
 
