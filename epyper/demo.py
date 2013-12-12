af253c32d9376aec319d5d47b50c68d81d33dcac
mnowotka <mnowotka@candela.(none)>
2013-12-12 15:11:36 +0000

    bugfix

--- epyper/demo.py
+++ epyper/demo.py
@@ -8,7 +8,7 @@ import itertools
 
 def toBytes(bits):
     return [sum([[0,1][byte[b]>0] << b for b in reversed(range(0,8))])
-               for byte in zip(*(iter(bits),) * 8)
+               for byte in reversed(zip(*(iter(bits),) * 8))
         ]
 
 def demo():
