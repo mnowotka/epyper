2a18bffbdd1df2dd46a43309f009b9062bd95e16
mnowotka <mnowotka@candela.(none)>
2013-12-13 13:21:49 +0000

    adding setup.py and stuff with some docs stub

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 epyper
 ======
 
-Python driver for Embedded Artists AB Embedded Artists 2.7 inch E-paper Display Module:
+Python driver for Embedded Artists 2.7 inch E-paper Display Module:
 
 http://www.embeddedartists.com/products/displays/lcd_27_epaper.php. 
 
 It's based on original C code available here:
 
 http://www.embeddedartists.com/sites/default/files/support/displays/epaper/epaper_pi_130307.tar.gz
+
+Rationale
+--------
+
+How to us it?
+--------
+
+Dependencies
+--------
+ * `WiringPi2-Python <https://github.com/WiringPi/WiringPi2-Python>`_
+ * `Pillow <https://github.com/python-imaging/Pillow>`_
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'mnowotka'
+
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='epyper',
+    version='0.1.0',
+    author='Michal Nowotka',
+    author_email='mmmnow@gmail.com',
+    description='Python driver for Embedded Artists 2.7 inch E-paper Display Module',
+    url='https://github.com/mnowotka/epyper',
+    license='MIT',
+    packages=['epyper'],
+    long_description=open('README.md').read(),
+    install_requires=['wiringpi2>=1.0.10',
+                      'Pillow>=2.2.1'],
+    include_package_data=False,
+    classifiers=['Development Status :: 4 - Beta',
+                 "Environment :: Handhelds/PDA's",
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Multimedia :: Video :: Display'],
+    zip_safe=False,
+)
